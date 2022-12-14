12617b89af9d2c272a67bb28d32c1d73a4cf7718
AngeloTripodi <tripodi.angelo@gmail.com>
2022-12-14 16:14:51 +0100

    Add obg list on dom

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log('aaa')
+const {createApp} = Vue
+
+createApp({
+    data(){
+        return{
+            index : 0,
+            toDoList : [
+                {
+                    text : 'Lezione Boolean @09:00',
+                    done : true
+                    }, {
+                        text : 'Call con Azienda',
+                        done : false
+                    }, {
+                        text : 'Portare a spasso il cane',
+                        done : true
+                    }, {
+                        text : 'Fare la spesa',
+                        done : false
+                    }, {
+                        text : 'Prenotare ristorante @Ristorante da Mario',
+                        done : true
+                    },
+            ],
+        }
+    },
+
+}).mount('#app')
